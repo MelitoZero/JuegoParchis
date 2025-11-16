27bf5d3b92160bcbcafcf567f272ff31e83ace65
Melito <premiumm9@gmail.com>
2025-11-16 12:43:04 -0700

    Primeras asociaciónes de controladores a vistas

--- app/src/main/java/juegoparchis/Controller/ControladorMenu.java
+++ app/src/main/java/juegoparchis/Controller/ControladorMenu.java
@@ -1,5 +1,21 @@
 package juegoparchis.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ControladorMenu {
     
+    @FXML
+    private Button btnCrearSala;
+    @FXML
+    private Button btnUnirse;
+    @FXML
+    protected void crearPartida() {
+
+    }
+    @FXML
+    protected void unirsePartida() {
+
+    }
+
 }
