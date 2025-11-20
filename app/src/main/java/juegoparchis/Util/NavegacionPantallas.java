a4575cbc4504674c123462a2a1de990b185d621f
Melito <premiumm9@gmail.com>
2025-11-19 21:51:38 -0700

    Nueva clase ayudante para navegación de pantallas

---
+++ app/src/main/java/juegoparchis/Util/NavegacionPantallas.java
@@ -0,0 +1,63 @@
+package juegoparchis.Util;
+
+import java.io.IOException;
+import java.net.URL;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class NavegacionPantallas {
+    //Método para iniciar la aplicación en la pantalla de inicio
+    public static void iniciarAplicacion(Stage stage, String rutaFXML, String tituloVentana) {
+        try {
+            // Cargar la nueva vista desde el archivo FXML especificado
+            URL fxmlURL = NavegacionPantallas.class.getResource(rutaFXML);
+            // Verificar si el recurso FXML fue encontrado y no es nulo
+            if (fxmlURL == null) {
+                System.out.println("Error: No se pudo encontrar el archivo FXML en la ruta especificada: " + rutaFXML);
+                return;
+            }
+            Parent nuevaVista = FXMLLoader.load(fxmlURL);
+            Scene nuevaEscena = new Scene(nuevaVista);
+            stage.setScene(nuevaEscena);
+            stage.setTitle(tituloVentana);
+            stage.show();
+        } catch (IOException e) {
+            //Maneja los errores y muestra mensajes en la consola
+            System.out.println("Error al iniciar la Ventana del juego:");
+            System.out.println("Tipo de error: " + e.getClass().getName());
+            System.out.println("Mensaje: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }        
+    //Métodos para la navegación entre pantallas 
+    public static void cambiarPantalla(ActionEvent event, String rutaFXML) {
+        try {
+             // Cargar la nueva vista desde el archivo FXML especificado
+            URL fxmlURL = NavegacionPantallas.class.getResource(rutaFXML);
+            // Verificar si el recurso FXML fue encontrado y no es nulo
+            if (fxmlURL == null) {
+                System.out.println("Error: No se pudo encontrar el archivo FXML en la ruta especificada: " + rutaFXML);
+                return;
+            }
+            // Carga la nueva vista
+            Parent nuevaVista = FXMLLoader.load(fxmlURL);
+            // Obtiene la escena actual y establece la nueva escena
+            Stage stageActual = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene nuevaEscena = new Scene(nuevaVista);
+            // Configurar la nueva escena en la ventana actual
+            stageActual.setScene(nuevaEscena);
+            //Muestra la nueva escena
+            stageActual.show();
+        } catch (IOException e) {
+            //Maneja los errores y muestra mensajes en la consola
+            System.out.println("Error al cargar la vista del juego:");
+            System.out.println("Tipo de error: " + e.getClass().getName());
+            System.out.println("Mensaje: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
