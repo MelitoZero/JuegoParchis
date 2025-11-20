67d4d2475f03d593e879a84ba29279ddecd0e079
Melito <premiumm9@gmail.com>
2025-11-19 22:54:25 -0700

    Cambios de  pantalla y navegación

---
+++ app/src/main/java/juegoparchis/Controller/ControladorPartida.java
@@ -0,0 +1,17 @@
+package juegoparchis.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControladorPartida {
+    
+    @FXML
+    private Button btnIniciar;
+    @FXML
+    protected void iniciarPartida( ActionEvent event) {
+        // Lógica para iniciar la partida
+    }
+    
+
+}
