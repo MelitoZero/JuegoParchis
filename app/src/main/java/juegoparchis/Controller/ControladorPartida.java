340702bac7ac9a2bbca7e03014442d44bb97edd8
Melito <premiumm9@gmail.com>
2025-11-25 00:42:57 -0700

    Funcionamiento dados

--- app/src/main/java/juegoparchis/Controller/ControladorPartida.java
+++ app/src/main/java/juegoparchis/Controller/ControladorPartida.java
@@ -1,9 +1,16 @@
 package juegoparchis.Controller;
 
+import java.io.InputStream;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.geometry.Point2D;
+import juegoparchis.Model.Dado;
 import juegoparchis.Model.Ficha;
 import juegoparchis.Model.Enum.Color;
 import juegoparchis.Util.CoordenadasTablero;
@@ -11,6 +18,15 @@ import juegoparchis.View.VistaFicha;
 
 public class ControladorPartida {
     
+    private Dado dadoModelo;
+    @FXML
+    private Label lblTurnoJugador;
+    @FXML
+    private Button btnTirarDado;
+    @FXML
+    private ImageView imgDado;
+    @FXML
+    private ImageView imgJugador1, imgJugador2, imgJugador3, imgJugador4;
     @FXML
     private ImageView imgPerfil1, imgPerfil2;
     @FXML
@@ -20,12 +36,26 @@ public class ControladorPartida {
     @FXML
     public void initialize() {
         // Aquí puedes agregar cualquier inicialización necesaria para el controlador
-        imgTablero.setOnMouseClicked(event ->{
+        /*imgTablero.setOnMouseClicked(event ->{
             double x = event.getX();
             double y = event.getY();
             System.out.println("recorrido.put(NUMERO, new Point2D(" + x + ", " + y + "));");    
-        });
+        });*/
+        dadoModelo = new Dado();
+        actualizarImgDado(dadoModelo.getValor());
         dibujarFichas();
+    }
+    @FXML
+    protected void lanzarDado(ActionEvent event) {
+        //Lógica para lanzar el dado
+        btnTirarDado.setDisable(true); // Deshabilitar el botón para evitar múltiples lanzamientos
+        // Simular el lanzamiento del dado
+        int valorDado = dadoModelo.lanzar();
+        System.out.println("Dado lanzado: " + valorDado);
+        // Actualizar la imagen del dado en la interfaz
+        actualizarImgDado(valorDado);
+        //Reactivar el botón despues de terminar el turno
+        //btnTirarDado.setDisable(false);
     }
     private void dibujarFichas() {
         // Lógica para dibujar las fichas en el panelFichas
@@ -47,6 +77,18 @@ public class ControladorPartida {
                     panelFichas.getChildren().add(vistaFicha);
                 }
             }
+        }
+    }
+    private void actualizarImgDado(int valor) {
+        // Lógica para actualizar la imagen del dado en la interfaz
+        try {
+            String rutaImagen = "/imgs/Dado" + valor + ".png";
+            InputStream flujo = getClass().getResourceAsStream(rutaImagen);
+            if (flujo != null) {
+                imgDado.setImage(new Image(flujo));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
